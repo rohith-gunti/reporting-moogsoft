eb9063acc2c8b0492dee919fc753752803782070
rohith-gunti <59819443+rohith-gunti@users.noreply.github.com>
2025-08-05 18:54:51 +0530

    Update config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+MOOGSOFT_API_KEY = os.getenv("MOOGSOFT_API_KEY")
+GMAIL_USER = os.getenv("GMAIL_USER")
+GMAIL_PASS = os.getenv("GMAIL_PASS")
+RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
